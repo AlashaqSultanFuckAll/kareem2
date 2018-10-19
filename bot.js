cd23fee87ab577f90e57e523d4209e0c97837a5c
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-19 21:17:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ client.on('message', message => {
 if (message.content === "startspam" ) {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 900000; x++) {
+      for(let x = 0; x < 200000; x++) {
         message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
           .then(m => {
             count++;
