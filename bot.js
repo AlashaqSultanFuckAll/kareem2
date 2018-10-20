282e141c4fdd32baa750c3ac9b1d5fbb362918ed
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-20 20:05:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,14 +26,6 @@ function timerFunc() {
 }
 
 
-if (command == "say") {
-let rank = message.guild.member(message.author).roles.find('name', '.');
-if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
-
 var timer = setTimeout(timerFunc, 1000);
 
 
